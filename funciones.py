f64c25a60fe34de898e8df96a3b6bf349fd6bd04
jennoely <jbaroni581@gmail.com>
2025-04-26 21:37:27 -0500

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,7 +1,6 @@
-import pygame as pyg
-heightq, widthq = 720, 480 
+import pygame as py
+height, width = 720, 480 
 
-font_large= pyg.font.SysFont("arial", 36)
 def size_img(background_original, screen):
-    ventana=screen.get_size()
-    return pyg.transform.scale(background_original, ventana)
+    ventana = screen.get_size()
+return py.transform.scale(background_original, ventana)
--- main.py
+++ main.py
@@ -1,32 +1,83 @@
-import pygame as pyg
+import pygame  as py
 import sys
 import funciones as fun
 
+# pygame setup
+py.init()
 
-pyg.init()
-screen = pyg.display.set_mode((fun.heightq, fun.widthq), pyg.RESIZABLE)
-pyg.display.set_caption("juego")
+screen = py.display.set_mode((fun.height, fun.width), py.RESIZABLE)
 
-background_original= pyg.image.load("img/images.jpg").convert()
+py.display.set_caption("juego")
 
-background_juego= fun.size_img(background_original, screen)
+background_original = py.image.load('img1.jpg').convert()
+
+background_juego=fun.size_img(background_original, screen)
+
 screen.blit(background_juego, (0,0))
 
-clock_fps = pyg.time.Clock()
+
+clock_fps = py.time.Clock()
 running = True
+dt = 0
+
+player_pos = py.Vector2(screen.get_width() / 4, screen.get_height() / 4)
+player_radio= 40
+
+triangle_height= 40
+triangle_base= 40
 
 while running:
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in py.event.get():
+        if event.type == py.QUIT:
             running = False
-        elif event.type == pyg.VIDEORESIZE:
-            background_juego= fun.size_img(background_juego, screen)
-    screen.blit(background_juego (0,0))
-    
-    pyg.display.flip()
-    pyg.display.update()
-    clock_fps.tick(60) # limits fps to 60
-    
-pyg.quit()
-sys.exit()
+        elif event.type == py.VIDEORESIZE:
+            background_juego= fun.size_img(background_original, screen)
 
+
+            screen.blit(background_juego, (0, 0))
+
+    punto1= (player_pos.x, player_pos.y - triangle_height / 2)
+    punto2= (player_pos.x - triangle_base / 2, player_pos.y + triangle_height / 2)
+    punto3= (player_pos.x + triangle_base / 2, player_pos.y + triangle_height / 2)
+
+    
+    py.draw.polygon(screen, "blue", [punto1, punto2, punto3])
+
+    keys = py.key.get_pressed()
+    if keys[py.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[py.K_s]:
+        player_pos.y += 300 * dt
+    if keys[py.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[py.K_d]:
+        player_pos.x += 300 * dt
+
+    screen_rect= screen.get_rect()
+
+
+    if player_pos.x - triangle_base <screen_rect.left:
+        player_pos.x = screen_rect.left + triangle_base
+
+    if player_pos.x + triangle_base > screen_rect.right:
+        player_pos.x = screen_rect.right - triangle_base
+
+    if player_pos.y - triangle_height < screen_rect.top:
+        player_pos.y = screen_rect.top + triangle_height
+    
+    if player_pos.y + triangle_height > screen_rect.bottom:
+        player_pos.y = screen_rect.bottom - triangle_height 
+
+
+    # flip() the display to put your work on screen
+    py.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock_fps.tick(500) / 1000
+
+py.quit()
+sys.exit()
